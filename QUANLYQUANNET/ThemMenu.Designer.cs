d106935f9b1f155fa0020c1f04ec209c65ac565a
GunterWi <132816424+GunterWi@users.noreply.github.com>
2023-05-07 18:54:00 +0700

    Add files via upload

---
+++ QUANLYQUANNET/ThemMenu.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace QUANLYQUANNET
+{
+    partial class ThemMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtma = new System.Windows.Forms.TextBox();
+            this.txtten = new System.Windows.Forms.TextBox();
+            this.txtsoluong = new System.Windows.Forms.TextBox();
+            this.txtdvt = new System.Windows.Forms.TextBox();
+            this.txtdongia = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cbbloai = new System.Windows.Forms.ComboBox();
+            this.btnthem = new System.Windows.Forms.Button();
+            this.btnthoat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtma
+            // 
+            this.txtma.Location = new System.Drawing.Point(136, 26);
+            this.txtma.Name = "txtma";
+            this.txtma.Size = new System.Drawing.Size(100, 22);
+            this.txtma.TabIndex = 0;
+            // 
+            // txtten
+            // 
+            this.txtten.Location = new System.Drawing.Point(136, 68);
+            this.txtten.Name = "txtten";
+            this.txtten.Size = new System.Drawing.Size(109, 22);
+            this.txtten.TabIndex = 1;
+            // 
+            // txtsoluong
+            // 
+            this.txtsoluong.Location = new System.Drawing.Point(136, 114);
+            this.txtsoluong.Name = "txtsoluong";
+            this.txtsoluong.Size = new System.Drawing.Size(34, 22);
+            this.txtsoluong.TabIndex = 2;
+            // 
+            // txtdvt
+            // 
+            this.txtdvt.Location = new System.Drawing.Point(350, 67);
+            this.txtdvt.Name = "txtdvt";
+            this.txtdvt.Size = new System.Drawing.Size(100, 22);
+            this.txtdvt.TabIndex = 3;
+            // 
+            // txtdongia
+            // 
+            this.txtdongia.Location = new System.Drawing.Point(350, 109);
+            this.txtdongia.Name = "txtdongia";
+            this.txtdongia.Size = new System.Drawing.Size(100, 22);
+            this.txtdongia.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Mã món";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Tên món";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Số lượng tồn";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(276, 70);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "DVT";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(276, 115);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 16);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Đơn giá";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(277, 28);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 16);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Loại hàng";
+            // 
+            // cbbloai
+            // 
+            this.cbbloai.FormattingEnabled = true;
+            this.cbbloai.Items.AddRange(new object[] {
+            "Nước uống",
+            "Đồ ăn"});
+            this.cbbloai.Location = new System.Drawing.Point(349, 24);
+            this.cbbloai.Name = "cbbloai";
+            this.cbbloai.Size = new System.Drawing.Size(121, 24);
+            this.cbbloai.TabIndex = 11;
+            // 
+            // btnthem
+            // 
+            this.btnthem.Location = new System.Drawing.Point(240, 156);
+            this.btnthem.Name = "btnthem";
+            this.btnthem.Size = new System.Drawing.Size(89, 38);
+            this.btnthem.TabIndex = 12;
+            this.btnthem.Text = "Thêm";
+            this.btnthem.UseVisualStyleBackColor = true;
+            this.btnthem.Click += new System.EventHandler(this.btnthem_Click);
+            // 
+            // btnthoat
+            // 
+            this.btnthoat.Location = new System.Drawing.Point(361, 156);
+            this.btnthoat.Name = "btnthoat";
+            this.btnthoat.Size = new System.Drawing.Size(89, 38);
+            this.btnthoat.TabIndex = 13;
+            this.btnthoat.Text = "Thoát";
+            this.btnthoat.UseVisualStyleBackColor = true;
+            // 
+            // ThemMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 206);
+            this.Controls.Add(this.btnthoat);
+            this.Controls.Add(this.btnthem);
+            this.Controls.Add(this.cbbloai);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtdongia);
+            this.Controls.Add(this.txtdvt);
+            this.Controls.Add(this.txtsoluong);
+            this.Controls.Add(this.txtten);
+            this.Controls.Add(this.txtma);
+            this.Name = "ThemMenu";
+            this.Text = "ThemMenu";
+            this.Load += new System.EventHandler(this.ThemMenu_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtma;
+        private System.Windows.Forms.TextBox txtten;
+        private System.Windows.Forms.TextBox txtsoluong;
+        private System.Windows.Forms.TextBox txtdvt;
+        private System.Windows.Forms.TextBox txtdongia;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cbbloai;
+        private System.Windows.Forms.Button btnthem;
+        private System.Windows.Forms.Button btnthoat;
+    }
+}
